5840696540bb30abb33134705583482cd5f7b263
wally <b.walocha@gmail.com>
2021-10-29 20:59:23 +0200

    canvas

--- src/wally.speedway.webapp/src/index.ts
+++ src/wally.speedway.webapp/src/index.ts
@@ -1,4 +1,47 @@
 import Track from "./models/track";
 
-const track = new Track();
-console.info('Wally.Speedway', track);
+// const track = new Track();
+// console.info('Wally.Speedway', track);
+
+window.onload = () => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d")!;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(600, 600);
+    ctx.stroke();
+    
+    track(ctx, 100, 100 - 50, 500, 250, 38 * Math.PI);
+
+    track(ctx, 0, 0, 700, 350, 54 * Math.PI);
+    
+    line(ctx, 350, 300, 350, 350);
+    
+    console.info('Window Loaded', ctx);
+}
+
+const track = (ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, radius: number) => {
+    ctx.beginPath();
+    ctx.moveTo(x,y+radius);
+    ctx.lineTo(x,y+h-radius);
+    ctx.arcTo(x,y+h,x+radius,y+h,radius);
+    ctx.lineTo(x+w-radius,y+h);
+    ctx.arcTo(x+w,y+h,x+w,y+h-radius,radius);
+    ctx.lineTo(x+w,y+radius);
+    ctx.arcTo(x+w,y,x+w-radius,y,radius);
+    ctx.lineTo(x+radius,y);
+    ctx.arcTo(x,y,x,y+radius,radius);
+    ctx.stroke();
+}
+
+const line = (ctx: CanvasRenderingContext2D, x0:number, y0:number, x1:number, y1:number) =>
+{
+    ctx.beginPath();
+    ctx.moveTo(x0,y0);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
+}
